497a1c3334fa17c48c38ddb393e17f84f45ee834
misterbao <mister.bao@gmail.com>
2019-02-17 07:57:01 +0000

    nomster 39

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,21 @@
+class PhotosController < ApplicationController
+before_action :authenticate_user!
+
+  def create
+    @place = Place.find(params[:place_id])
+    @place.photos.create(photo_params.merge(user: current_user))
+    redirect_to place_path(@place)
+  end
+
+    def show
+    @place = Place.find(params[:id])
+    @photo = Photo.new
+  end 
+
+
+ private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :picture)
+  end 
+end
